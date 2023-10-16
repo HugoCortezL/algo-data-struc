0f59d560ffce989c8af09b0d362b0b1e2377516a
Hugo Cortez <hugocortezprog@gmail.com>
2023-10-16 14:16:30 -0300

    Feat: Adicionado linear_search e binary_search

---
+++ Algorithms/Arrays/__init__.py
@@ -0,0 +1,10 @@
+from .linear_search import linear_search
+from .binary_search import binary_search, recursive_binary_search
+
+__doc__ = "All arrays algorithms"
+
+__all__ = [
+    linear_search,
+    binary_search,
+    recursive_binary_search
+]
