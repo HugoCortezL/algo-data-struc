e21b579951be3818752ce1893b8f2bff9cfc6906
Hugo Cortez <hugocortezprog@gmail.com>
2023-10-17 14:15:13 -0300

    Feat: Adicionado merge_sort

---
+++ Algorithms/Arrays/merge_sort.py
@@ -0,0 +1,31 @@
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left_half = array[:mid] 
+        right_half = array[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                array[k] = left_half[i]
+                i += 1
+            else:
+                array[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            array[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            array[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return array
